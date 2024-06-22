ca052037a855118a84a0685555451736fd74cca9
hashiiiii <pkj59wwj@gmail.com>
2024-06-23 00:34:08 +0900

    fix: structure

--- src/hono.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'hono' {
-    interface ContextRenderer {
-        (content: string | Promise<string>, head: { title: string }): Response | Promise<Response>
-    }
-}
---
+++ types/hono.d.ts
@@ -0,0 +1,10 @@
+import {} from 'hono';
+
+declare module 'hono' {
+    interface ContextRenderer {
+        (
+            content: string | Promise<string>,
+            head: { title: string },
+        ): Response | Promise<Response>;
+    }
+}
