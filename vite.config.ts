9e2c9d8dd21e92e02f57767676ae371042246f82
hashiiiii <pkj59wwj@gmail.com>
2024-06-23 20:28:52 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
-import ssg from '@hono/vite-ssg'
 import honox from 'honox/vite'
 
 const entry = 'app/server.ts'
 
 export default defineConfig({
     plugins: [
-        honox(),
-        ssg({ entry })
+        honox({ entry }),
     ]
 })
