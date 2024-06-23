82313bc4742256a86ebc9f40cc4fe3e9c6a1b8af
hashiiiii <pkj59wwj@gmail.com>
2024-06-23 13:15:58 +0900

    update: deploy flow

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import ssg from '@hono/vite-ssg'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 const entry = './pages/index.tsx'
-const src = 'public/**/*'
-const dest = 'dist'
+const staticSource = 'public/**/*'
+const staticDestination = 'dist'
 
 export default defineConfig({
     plugins: [
@@ -12,8 +12,8 @@ export default defineConfig({
         viteStaticCopy({
             targets: [
                 {
-                    src: src,
-                    dest: dest,
+                    src: staticSource,
+                    dest: staticDestination,
                     overwrite: false
                 }
             ]
