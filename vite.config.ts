403223535e6288394d6e1263a98f6acc602580ed
hashiiiii <pkj59wwj@gmail.com>
2024-06-23 18:28:20 +0900

    fix: test

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
+//import { viteStaticCopy } from 'vite-plugin-static-copy'
 import ssg from '@hono/vite-ssg'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import honox from 'honox/vite'
+
+const entry = './app/server.ts'
 
 export default defineConfig({
     plugins: [
-        ssg({ entry: './pages/index.tsx' }),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'public/**/*',
-                    dest: 'public/static',
-                    overwrite: false
-                }
-            ]
-        })
+        honox(),
+        ssg({ entry })
+        // viteStaticCopy({
+        //     targets: [
+        //         {
+        //             src: 'public/**/*',
+        //             dest: 'public/static',
+        //             overwrite: false
+        //         }
+        //     ]
+        // })
     ],
     build: {
         outDir: 'public',
