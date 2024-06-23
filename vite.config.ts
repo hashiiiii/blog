227f0e533814eecbfcaee2d775484e0bb627d37e
hashiiiii <pkj59wwj@gmail.com>
2024-06-23 12:49:07 +0900

    add: test data

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import ssg from '@hono/vite-ssg'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+const entry = './pages/index.tsx'
 
 export default defineConfig({
-    plugins: [ssg({
-        entry: './pages/index.tsx',
-    })],
+    plugins: [
+        ssg({ entry }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'public/**/*',
+                    dest: 'dict',
+                    overwrite: false
+                }
+            ]
+        })
+    ],
 })
